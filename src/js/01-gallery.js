025dca1fcaed4a8713ac9c74c091dd8cc733ecd8
Lyuba_o <lyubao7749@gmail.com>
2022-05-31 20:52:16 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import  {galleryItems} from './gallery-items';
 
 console.log(galleryItems);
+
+const refs = {
+    galleryContainer: document.querySelector(".gallery"),
+ };
+
+ const imagesTemplate = ({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a> `;
+  };
+ 
+const addImage = galleryItems.map(imagesTemplate).join("");
+refs.galleryContainer.insertAdjacentHTML("afterbegin", addImage);
+
+const lightbox = new SimpleLightbox(".gallery .gallery__item", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
